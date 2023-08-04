fd370ea6b15f7e710aed527686134a205b7e753a
HYSA <aj.anunciado@gmail.com>
2023-08-04 20:04:07 +0800

    asd

--- index.js
+++ index.js
@@ -168,3 +168,17 @@ greetingEl.textContent+=sentence[i];
 
 }
 }
+
+let playerone=102;
+let playertwo=103;
+
+function getFast(){
+if(playerone<playertwo){
+    return playerone;
+    console.log("player one wins")
+}else if(playerone>playertwo){ 
+    return playertwo;
+console.log("player two wins");
+}
+}
+getFast()
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,14 @@
-let j=0;
-let cards = [7, 3, 9]
 
-// Create a for loop that logs out all the cards in the array
-// Use cards.length to specify how long the loop should run
- for(j=0;j<cards.length;j++){
-    console.log(cards[j]);
- }
+let playerone=102;
+let playertwo=103;
+
+function getFast(){
+if(playerone<playertwo){
+    return playerone;
+    console.log("player one wins")
+}else if(playerone>playertwo){ 
+    return playertwo;
+console.log("player two wins");
+}
+}
+getFast()
