388b27bb34efd3c450e42b70c8ac15708700c8f7
HYSA <aj.anunciado@gmail.com>
2023-08-09 04:38:19 +0800

    aaaaaaaaaaaa

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -5,10 +5,8 @@ let hands = ["rock", "paper", "scissor"]
 const randomHands = () => {
     for(let i=0; i< hands.length;i++){
 let index=Math.floor(Math.random()*3) 
-// console.log(hands[i])
 return hands[index]
     }
-    //  return ["rock","paper","scissor"] [hands]
 }
 let outcome=randomHands()
 console.log(outcome)
