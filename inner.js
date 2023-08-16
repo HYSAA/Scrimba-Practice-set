6c89958dc2578e82da26c85ddd7bf2b6ceba7779
HYSA <aj.anunciado@gmail.com>
2023-08-17 03:57:33 +0800

    a

--- inner.js
+++ inner.js
@@ -1,4 +1,14 @@
 
 const see= document.getElementById("clicks")
+const saw=document.getElementById('saw')
 
-see.innerHTML="<button>Buy!</button>"
+    see.innerHTML="<button onclick='buy()'>Buy!</button>"
+
+    let buy = () =>{
+        see.innerHTML+="<p> Thanks for the purchase </p>"
+
+    }
+
+
+
+
