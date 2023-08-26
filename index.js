17f51b79ad74785de5099568aa0b615c72244e09
HYSA <aj.anunciado@gmail.com>
2023-08-26 20:19:13 +0800

    asdasdas

--- index.js
+++ index.js
@@ -415,3 +415,9 @@ Cheers,${sender}`
 
 console.log(email)
 
+console.log(  Boolean("")   ) //false
+console.log(  Boolean("0")  ) //false
+console.log(  Boolean(100)  ) //true
+console.log(  Boolean(null) ) //false
+console.log(  Boolean([0])  ) //false
+console.log(  Boolean(-0)   ) //false
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,1 @@
-const recipient = "James"
-const sender="aj"
-// Refactor the email string to use template strings
-// const email = "Hey " + recipient + "! How is it going? Cheers Per"
-//template strings
-const email=`Hey 
-${recipient}! 
-How is it going? 
-Cheers,${sender}`
-
-console.log(email)
+console.log(  Boolean([0])  ) //false
