7bc9ffe3d756e5b367c2c14b033249ff1d9ed749
HYSA <aj.anunciado@gmail.com>
2023-08-27 20:35:27 +0800

    asdsad

--- parameter.js
+++ parameter.js
@@ -15,3 +15,16 @@ greetUser("Hello there", "Aj")
 
 
 })
+
+
+// Create a function, add(), that adds two numbers together and returns the sum
+
+const add = (x,y) => {
+  return x+y;
+
+}
+
+
+console.log( add(3,4)    ) // should log 7
+console.log( add(9, 102) ) // should log 111
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,10 @@
-console.log(  Boolean([0])  ) //false
+
+const add = (x,y) => {
+  return x+y;
+
+}
+
+
+console.log( add(3,4)    ) // should log 7
+console.log( add(9, 102) ) // should log 111
+
