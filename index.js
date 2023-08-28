ff4e85b05bef1d7904cee3cf507ef3b526c1443b
HYSA <aj.anunciado@gmail.com>
2023-08-28 20:53:15 +0800

    ASDASD

--- index.js
+++ index.js
@@ -444,3 +444,6 @@ const subjects=(aj) =>{
 
 subjects(myCourses)
 
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
--- parameter.js
+++ parameter.js
@@ -15,29 +15,36 @@ greetUser("Hello there", "Aj")
 
 
 })
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+// localStorage.setItem("message","I miss uQQ")
+let message=localStorage.getItem("message")
+console.log(message)
 
 
-// Create a function, add(), that adds two numbers together and returns the sum
+// // Create a function, add(), that adds two numbers together and returns the sum
 
-const add = (x,y) => {
-  return x+y;
+// const add = (x,y) => {
+//   return x+y;
 
-}
-
-
-console.log( add(3,4)    ) // should log 7
-console.log( add(9, 102) ) // should log 111
-
-// Create a function, getFirst(arr), that returns the first item in the array
-const getFirst=(arr)=>{
-
-return arr[0]
+// }
 
 
-}
+// console.log( add(3,4)    ) // should log 7
+// console.log( add(9, 102) ) // should log 111
 
-let card=getFirst([10,20,30])
-console.log(card)
+// // Create a function, getFirst(arr), that returns the first item in the array
+// const getFirst=(arr)=>{
+
+// return arr[0]
+
+
+// }
+
+// let card=getFirst([10,20,30])
+// console.log(card)
 
 
 // Call it with an array as an argument to verify that it works
